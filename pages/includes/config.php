9c28fafec04f3458b407889e7ec8026b7028d421
jing <n112140@gmail.com>
2025-01-13 09:14:46 +0800

    更新database連接資料

--- pages/includes/config.php
+++ pages/includes/config.php
@@ -3,8 +3,8 @@
 const DB_HOST = '127.0.0.1';
 const DB_USER = 'root';
 const DB_PASS = 'P@ssw0rd';
-const DB_NAME = 'gym02';
+const DB_NAME = 'gym_database';
 const DB_PORT = 3306;
 
 
-const PATH_BASE = '期中\gym--main\MFEE59MID-main';
+const PATH_BASE = 'gym-report-main';
