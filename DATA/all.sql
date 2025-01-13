7251be82c62881e6b1d14efb6b2215800f3b786a
yijing <n112140@gmail.com>
2025-01-13 23:25:53 +0800

    據點電話更新

--- DATA/all.sql
+++ DATA/all.sql
@@ -87,11 +87,11 @@ CREATE TABLE gyms (
 -- 插入資料，營業星期作為逗號分隔字串
 INSERT INTO gyms (name, address, business_days, opening_hours, closing_hours, description, contact_info, email, manager, image_url, google_map_link)
 VALUES
-('健身房A', '台南市中華東路100號', 'Monday, Tuesday, Wednesday, Thursday, Friday', '08:00:00', '22:00:00', '提供各類健身器材，適合健身初學者與進階者使用，並提供專業指導課程。', '06-1234-5678', 'contactA@gym.com', '王大明', 'https://example.com/images/gymA.jpg', 'https://goo.gl/maps/abc123'),
-('健身房B', '台南市永康區中正路200號', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', '07:00:00', '23:00:00', '擁有先進的重訓設備及寬敞的游泳池，設有個人化健身計劃設計服務。', '06-2345-6789', 'contactB@gym.com', '李小華', 'https://example.com/images/gymB.jpg', 'https://goo.gl/maps/def456'),
-('健身房C', '台南市安平區建國路300號', 'Tuesday, Wednesday, Thursday, Friday, Saturday', '06:30:00', '21:30:00', '提供多樣的有氧運動課程及高效的跑步機，環境清新舒適。', '06-3456-7890', 'contactC@gym.com', '張志強', 'https://example.com/images/gymC.jpg', 'https://goo.gl/maps/ghi789'),
-('健身房D', '台南市北區成功路400號', 'Monday, Wednesday, Friday, Saturday', '09:00:00', '20:00:00', '專業拳擊訓練課程、自由重訓區，專注於力量與耐力的提升。', '06-4567-8901', 'contactD@gym.com', '陳美麗', 'https://example.com/images/gymD.jpg', 'https://goo.gl/maps/jkl012'),
-('健身房E', '台南市西區府前路500號', 'Monday, Tuesday, Wednesday, Thursday, Friday', '08:00:00', '22:30:00', '提供各式團體課程及無氧設備，讓您更有效率地達成健身目標。', '06-5678-9012', 'contactE@gym.com', '林俊杰', 'https://example.com/images/gymE.jpg', 'https://goo.gl/maps/mno345');
+('健身房A', '台南市中華東路100號', 'Monday, Tuesday, Wednesday, Thursday, Friday', '08:00:00', '22:00:00', '提供各類健身器材，適合健身初學者與進階者使用，並提供專業指導課程。', '06-1234-567', 'contactA@gym.com', '王大明', 'https://example.com/images/gymA.jpg', 'https://goo.gl/maps/abc123'),
+('健身房B', '台南市永康區中正路200號', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', '07:00:00', '23:00:00', '擁有先進的重訓設備及寬敞的游泳池，設有個人化健身計劃設計服務。', '06-2345-678', 'contactB@gym.com', '李小華', 'https://example.com/images/gymB.jpg', 'https://goo.gl/maps/def456'),
+('健身房C', '台南市安平區建國路300號', 'Tuesday, Wednesday, Thursday, Friday, Saturday', '06:30:00', '21:30:00', '提供多樣的有氧運動課程及高效的跑步機，環境清新舒適。', '06-3456-789', 'contactC@gym.com', '張志強', 'https://example.com/images/gymC.jpg', 'https://goo.gl/maps/ghi789'),
+('健身房D', '台南市北區成功路400號', 'Monday, Wednesday, Friday, Saturday', '09:00:00', '20:00:00', '專業拳擊訓練課程、自由重訓區，專注於力量與耐力的提升。', '06-4567-890', 'contactD@gym.com', '陳美麗', 'https://example.com/images/gymD.jpg', 'https://goo.gl/maps/jkl012'),
+('健身房E', '台南市西區府前路500號', 'Monday, Tuesday, Wednesday, Thursday, Friday', '08:00:00', '22:30:00', '提供各式團體課程及無氧設備，讓您更有效率地達成健身目標。', '06-5678-901', 'contactE@gym.com', '林俊杰', 'https://example.com/images/gymE.jpg', 'https://goo.gl/maps/mno345');
 
 -- 文章列表 
 CREATE TABLE articles (
