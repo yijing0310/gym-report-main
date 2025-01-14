afc3621eeb909099f5a4485b621e868839f624f3
jing <n112140@gmail.com>
2025-01-14 11:38:48 +0800

    新增bar圖像導回home

--- pages/includes/html-sidebar.php
+++ pages/includes/html-sidebar.php
@@ -21,7 +21,7 @@ if (! isset($pageName)) {
 
   
   <div class="app-brand demo ">
-    <a href="index.html" class="app-brand-link">
+    <a href="home.php" class="app-brand-link">
     <span class="app-brand-logo demo">
 
       <svg width="25" viewBox="0 0 25 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
