fe0ed3b037a2ae1487cd87642fac7e7408ea9005
jing <n112140@gmail.com>
2025-01-14 10:31:33 +0800

    header新增標題

--- pages/includes/html-header.php
+++ pages/includes/html-header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title></title>
+    <title><?=isset($title)?$title:'gym管系統'?></title>
 
     
     <meta name="description" content="Most Powerful &amp; Comprehensive Bootstrap 5 Admin Dashboard built for developers!" />
